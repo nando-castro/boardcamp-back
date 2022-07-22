9251b2538dbd29eb16c0a967985bbb9fbf0d7ebe
nando-castro <fernando.castro10.master@gmail.com>
2022-07-21 22:58:33 -0300

    fix put customers

--- src/controllers/customersControllers.js
+++ src/controllers/customersControllers.js
@@ -40,10 +40,19 @@ export async function updateClient(req, res) {
     const { name, phone, cpf, birthday } = req.body;
     const { id } = req.params;
 
-    await connection.query(
+    const clientExists = await connection.query(
+      `SELECT * FROM customers WHERE cpf = $1`,
+      [cpf]
+    );
+
+    if (clientExists.rows.length > 0) {
+      return res.sendStatus(409);
+    }
+    const client = await connection.query(
       `UPDATE customers SET name = $1, phone = $2, cpf = $3, birthday = $4 WHERE id = $5`,
       [name, phone, cpf, birthday, id]
     );
+
     res.sendStatus(200);
   } catch (error) {
     res.sendStatus(500);
